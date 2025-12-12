5603b7d508bec35e91ec1ff0f2a676764ebf02b6
Lancer385 <ahmedkhalaf1234123@gmail.com>
2025-12-12 15:36:00 +0200

    feat: ship axis, decide wether the ship is vertical or horizontal

--- src/module/ship.js
+++ src/module/ship.js
@@ -1,12 +1,25 @@
+const horizontal = 0;
+const vertical = 1;
+
 export class Ship {
-    constructor(name, length){
+    constructor(name, length , axis){
         this.name = name;
         this.length = length;
         this.hit = 0;
+        this.axis = axis;
+    }
+    
+    isHorizontal(){
+        return this.axis === horizontal;
+    }
+
+    changePosition(){
+       this.axis = this.axis === horizontal ? vertical : horizontal;
     }
     isSunk(){
         return this.length <= this.hit;
     }
+
     gotHit(){
         this.hit += 1;
     }
