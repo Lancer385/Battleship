783fb7772b1844468c28d65a9da0fa7f414de4d9
Lancer385 <ahmedkhalaf1234123@gmail.com>
2025-12-13 17:41:13 +0200

    chore: update configuration

--- src/module/ship.js
+++ src/module/ship.js
@@ -2,27 +2,27 @@ const horizontal = 0;
 const vertical = 1;
 
 export class Ship {
-    constructor(name, length , axis){
-        this.name = name;
-        this.length = length;
-        this.hit = 0;
-        this.coordinates = [];
-        this.axis = axis;
-        this.id;
-    }
-    
-    isHorizontal(){
-        return this.axis === horizontal;
-    }
+  constructor(name, length, axis) {
+    this.name = name;
+    this.length = length;
+    this.hit = 0;
+    this.coordinates = [];
+    this.axis = axis;
+    this.id;
+  }
 
-    changePosition(){
-       this.axis = this.axis === horizontal ? vertical : horizontal;
-    }
-    isSunk(){
-        return this.length <= this.hit;
-    }
+  isHorizontal() {
+    return this.axis === horizontal;
+  }
 
-    gotHit(){
-        this.hit += 1;
-    }
+  changePosition() {
+    this.axis = this.axis === horizontal ? vertical : horizontal;
+  }
+  isSunk() {
+    return this.length <= this.hit;
+  }
+
+  gotHit() {
+    this.hit += 1;
+  }
 }
