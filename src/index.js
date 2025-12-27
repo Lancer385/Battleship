45f79336e2f84fdf0f2a40ab6009881038d53c6e
Lancer385 <ahmedkhalaf1234123@gmail.com>
2025-12-27 04:01:14 +0200

    add: visual feedback for game flow (ship,hit,miss) add: 3 more ships to the board.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import { DOM } from "./UI/DOM.js"
+import { GameController } from "./module/game-controller.js";
 
-const game = new DOM();
-
-game.submit();
-game.viewShips();
-game.placeShip();
-game.randomizer();
-game.startGame()
+const game = new GameController();;
+const UI = new DOM(game);
+UI.submit();
+UI.viewShips();
+UI.placeShip();
+UI.randomizer();
+UI.startGame()
